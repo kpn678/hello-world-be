d13d69835947755fa82971508ab5748be583891f
Kristy Nguyen <kristypnguyen7@gmail.com>
2023-01-11 14:33:18 -0700

    Import env vars

--- services/harper-save-message.js
+++ services/harper-save-message.js
@@ -1,5 +1,7 @@
-const harperSaveMessage = () => {
-
+const harperSaveMessage = (message, username, room) => {
+  const dbUrl = process.env.HARPERDB_URL;
+  const dbPw = process.env.HARPERDB_PW;
+  if (!dbPw || !dbUrl) return null;
 }
 
 module.exports = harperSaveMessage;
