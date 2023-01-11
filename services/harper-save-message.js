4616caea0464ca5699491d4dccbde312a6e65f3a
Kristy Nguyen <kristypnguyen7@gmail.com>
2023-01-11 14:37:34 -0700

    Convert data to JSON string

--- services/harper-save-message.js
+++ services/harper-save-message.js
@@ -2,6 +2,19 @@ const harperSaveMessage = (message, username, room) => {
   const dbUrl = process.env.HARPERDB_URL;
   const dbPw = process.env.HARPERDB_PW;
   if (!dbPw || !dbUrl) return null;
+
+  let data = JSON.stringify({
+    operation: 'insert',
+    schema: 'realtime_chat_app',
+    table: 'messages',
+    records: [
+      {
+        message,
+        username,
+        room
+      }
+    ]
+  });
 }
 
 module.exports = harperSaveMessage;
