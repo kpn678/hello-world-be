a177a669354d3874ec5733399efd7c1224de963c
Kristy Nguyen <kristypnguyen7@gmail.com>
2023-01-12 13:11:06 -0700

    Integrate env vars into fn

---
+++ services/harper-get-message.js
@@ -0,0 +1,9 @@
+let axios = require('axios');
+
+const harperGetMessage = (room) => {
+  const dbUrl = process.env.HARPERDB_URL;
+  const dbPw= process.env.HARPERDB_PW;
+  if (!dbPw || !dbUrl) return null;
+}
+
+module.exports = harperGetMessage;
